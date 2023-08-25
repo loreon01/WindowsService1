dcdb6c4b970f92c128bd8a55157f2230ab85430c
Lorenzo Manzo <118081043+loreon01@users.noreply.github.com>
2023-08-25 11:59:58 -0300

    Add files via upload

---
+++ WindowsService1_Lorenzo_Manzo/IncrementService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace WindowsService1_Lorenzo_Manzo
+{
+    public class IncrementService : ServiceBase
+    {
+        private Timer timer;
+        private int counter;
+
+        public IncrementService()
+        {
+            ServiceName = "IncrementService";
+            CanStop = true;
+            CanPauseAndContinue = true;
+            CanPauseAndContinue = true;
+            AutoLog = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Inicializar el contador
+            counter = 0;
+
+            // Crear el archivo txt si no existe
+            string path = @"E:\Increment.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+
+            // Inicializar el timer con un intervalo de 1 segundo
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += new ElapsedEventHandler(OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            //Detener el timer
+            timer.Stop();
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
+            // Incrementar el contador
+            counter++;
+
+            // Escribir el contador en el archivo txt
+            string path = @"E:\Increment.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(counter);
+            }
+        }
+
+    }
+}
---
+++ WindowsService1_Lorenzo_Manzo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1_Lorenzo_Manzo
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        static void Main()
+        {
+            // Crear una instancia del servicio
+            IncrementService service = new IncrementService();
+
+            //Ejecutar el servicio
+            ServiceBase.Run(service);
+
+        }
+    }
+}
